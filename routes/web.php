ec097cc07cefca7354859eea6d725b9c6fc8c859
Krotov Anatoliiy <tolumbas@gmail.com>
2021-12-04 20:26:38 +0200

    'Initial'

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+<<<<<<< HEAD
 use \App\Http\Controllers\BlogController;
+=======
+
+>>>>>>> 33195fe ('Initial')
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +17,12 @@ use \App\Http\Controllers\BlogController;
 |
 */
 
+<<<<<<< HEAD
 Route::get('/', [BlogController::class, 'index']);
 Route::get('/category/{slug}', [BlogController::class, 'getPostsByCategory'])->name('getPostsByCategory');
 Route::get('/category/{slug_category}/{slug_post}', [BlogController::class, 'getPost'])->name('getPost');
+=======
+Route::get('/', function () {
+    return view('welcome');
+});
+>>>>>>> 33195fe ('Initial')
