ea1e3975b02d9b76ffc7eeec958a68458849782d
Krotov Anatoliiy <tolumbas@gmail.com>
2021-12-12 21:44:05 +0000

    update: Added Category and Post Seeders calls into DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+		(new CategorySeeder)->run();
+		(new PostSeeder)->run();
     }
 }
